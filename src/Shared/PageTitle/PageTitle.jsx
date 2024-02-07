c20537df5ac176f6c91ab1449a84514df1e67f86
ZIHAD22 <mdzihad2244@gmail.com>
2024-02-07 20:16:25 +0600

    add home page title

--- src/Shared/PageTitle/PageTitle.jsx
+++ src/Shared/PageTitle/PageTitle.jsx
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet-async";
 const PageTitle = ({ title }) => {
   return (
     <Helmet>
-      <title>{title}</title>
+      <title>{`Bistro Boss | ${title}`}</title>
     </Helmet>
   );
 };
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
+import PageTitle from "../../../Shared/PageTitle/PageTitle";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 
 const Home = () => {
   return (
     <div>
+      <PageTitle title="Home" />
       <Banner />
       <Category />
     </div>
