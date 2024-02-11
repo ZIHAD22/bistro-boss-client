7dcf12323f1cba0594e593b8493a322a76a0f7dd
ZIHAD22 <mdzihad2244@gmail.com>
2024-02-11 20:43:15 +0600

    clean testimonial file

--- src/pages/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -1,36 +1,7 @@
-import { Rating } from "@smastrom/react-rating";
 import SectionTitle from "../../../Shared/SectionTitle/SectionTitle";
 import RatingComponent from "../../../Shared/RatingComponent/RatingComponent";
 
 const Testimonial = () => {
-  const renderStars = (rating) => {
-    const stars = [];
-    for (let i = 0; i < 5; i++) {
-      if (i < rating) {
-        stars.push(
-          <svg
-            key={i}
-            className="w-5 h-5 fill-current text-yellow-500"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18l-4-4 1.41-1.41L12 16.17l6.59-6.59L18 10l-6 6z" />
-          </svg>
-        );
-      } else {
-        stars.push(
-          <svg
-            key={i}
-            className="w-5 h-5 fill-current text-gray-400"
-            viewBox="0 0 24 24"
-          >
-            <path d="M0 0h24v24H0z" fill="none" />
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-16c-4.41 0-8 3.59-8 8s3.59 8 8 8 8-3.59 8-8-3.59-8-8-8z" />
-          </svg>
-        );
-      }
-    }
-    return stars;
-  };
   return (
     <div className="my-8">
       <SectionTitle
