fac9eaed4b37e4a3e4bb8cd61f893d013d439c96
ZIHAD22 <mdzihad2244@gmail.com>
2024-02-12 14:26:03 +0600

    start menu page code

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -1,31 +1,33 @@
+import { NavLink } from "react-router-dom";
+import { router } from "../../Routes/Routes";
+
 const NavBar = () => {
   const navOptions = (
     <>
-      <li>
-        <a>Item 1</a>
+      <li className={`mr-3 hover:text-[#EEFF25]`}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "text-[#EEFF25]" : "")}
+        >
+          Home
+        </NavLink>
       </li>
       <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li>
-              <a>manu</a>
-            </li>
-            <li>
-              <a>Submenu 2</a>
-            </li>
-          </ul>
-        </details>
-      </li>
-      <li>
-        <a>Item 3</a>
+        <NavLink
+          to="/menu"
+          className={({ isActive }) => (isActive ? "text-[#EEFF25]" : "")}
+        >
+          Menu
+        </NavLink>
       </li>
     </>
   );
 
+  console.log(router.path);
+
   return (
     <div>
-      <div className="navbar fixed max-w-screen-2xl z-10 bg-opacity-10  bg-black  text-white">
+      <div className="navbar fixed max-w-screen-2xl z-10 bg-opacity-50  bg-black  text-white">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -54,7 +56,7 @@ const NavBar = () => {
           <a className="btn btn-ghost text-xl">Bistro Boss</a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">{navOptions}</ul>
+          <ul className="menu-horizontal px-1">{navOptions}</ul>
         </div>
         <div className="navbar-end">
           <a className="btn">Button</a>
